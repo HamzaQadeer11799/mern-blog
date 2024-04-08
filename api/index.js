5dc96013803a21db2a9c9bc367f6aff5916ec44e
Hamza Qadeer <hamza@qadeer.com>
2024-04-08 04:38:59 -0700

    connect to the database

--- api/index.js
+++ api/index.js
@@ -1,6 +1,18 @@
 import express from 'express';
+import mongoose from 'mongoose';
+import dotenv from 'dotenv';
 const app = express();
+dotenv.config();
 
 app.listen(3000, () => {
   console.log('Server is running on port 3000! ');
 });
+
+mongoose
+  .connect(process.env.MONGO)
+  .then(() => {
+    console.log('mongoDb is connected');
+  })
+  .catch((err) => {
+    console.log(err);
+  });
