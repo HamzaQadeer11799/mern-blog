da3b7e6adf199e20c3e09cfc06e29b6f2adf1b22
Hamza Qadeer <hamza@qadeer.com>
2024-04-08 11:33:13 -0700

    made little change in user model

--- api/models/user.model.js
+++ api/models/user.model.js
@@ -3,21 +3,21 @@ const userSchema = new mongoose.Schema(
   {
     username: {
       type: String,
-      required: true, // required means no user will be created without having username
-      unique: true, //every user should have unique user name
+      required: true,
+      unique: true,
     },
     email: {
       type: String,
-      required: true, // required means no user will be created without having username
+      required: true,
       unique: true,
     },
     password: {
       type: String,
-      required: true, //we didn't made password field as unique bcoz two users have same password ok!
+      required: true,
     },
   },
   { timestamps: true }
-); //timestamps true save the time of creation and updation of new user ok!
+);
 
 const User = mongoose.model('User', userSchema);
 export default User;
