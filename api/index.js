e2a2482f6f8006a02192af627325e60b25bbccdc
Hamza Qadeer <hamza@qadeer.com>
2024-05-25 12:15:05 -0700

    added pre installation

--- api/index.js
+++ api/index.js
@@ -6,6 +6,7 @@ import userRoutes from './routes/user.route.js';
 import postRoutes from './routes/post.route.js';
 import authRoutes from './routes/auth.route.js';
 import commentRoutes from './routes/comment.route.js';
+import path from 'path';
 const app = express();
 app.use(express.json());
 app.use(cookieParser());
@@ -24,11 +25,17 @@ mongoose
     console.log(err);
   });
 
+const __dirname = path.resolve();
 app.use('/api/user', userRoutes);
 app.use('/api/auth', authRoutes);
 app.use('/api/post', postRoutes);
 app.use('/api/comment', commentRoutes);
 
+app.use(express.static(path.join(__dirname, '/client/dist')));
+
+app.get('*', (req, res) => {
+  res.sendFile(path.join(__dirname, 'client', 'dist', 'index.html'));
+});
 app.use((err, req, res, next) => {
   const statusCode = err.statusCode || 500;
   const message = err.message || 'Internal Server Error';
